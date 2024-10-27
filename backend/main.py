a6aade1eab168bc413bd379876116af82b67bf5f
Joel Kalai <joelkalai@Joels-MacBook-Air.local>
2024-10-27 11:42:48 -0400

    working webpage

--- backend/main.py
+++ backend/main.py
@@ -6,37 +6,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    with open("../event/events.json", "r") as f:
+    with open("../frontend/index.html", "r") as f:
         return f.read()
 
+@app.route("/get-events")
+def get_events():
+    with open("../events/events.json", "r") as f:
+        return f.read()
 
-@app.route("/events", methods=["GET", "POST", "DELETE"])
-def events():
+@app.route("/modify-events", methods=["POST", "DELETE"])
+def modify_events():
     with open("../events/events.json", "r") as f:
         data = json.loads(f.read())
 
     if request.method == "POST":
         # basically request.form is the data so we manually add to data dictionary and add
         # to events file
-        for key in request.form:
-            data[key] = request.form[key]
+        new_data = json.loads(request.data)
+        data.append(new_data)
         with open("../events/events.json", "w") as f:
             f.write(json.dumps(data))
         return "success"
-    elif request.method == "GET":
-        # some checks and then return the event associated with request.args["name"]
-        if "name" not in request.args:
-            return "No name in get method"
-        name = request.args["name"]
-        if name not in data:
-            return "No matching event to get"
-        return data[request.args["name"]]
     elif request.method == "DELETE":
         # some checks and then delete the event associated with request.args["name"]
-        if request.args["name"] not in data:
-            return "No matching event to remove"
-        data.pop(request.args["name"])
-        with open("../events/events.json", "w") as f:
-            f.write(json.dumps(data))
+        data.remove(request.data)
         return "success"
 
--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,5 @@
 import requests
 
-url = "http://127.0.0.1:5000/events"
+url = "http://127.0.0.1:5000/modify-events"
 
-requests.post(url, {"another event": "event description"})
-print(requests.get(url, params={"name": "another event"}).text)
-requests.delete(url, params={"name": "another event"})
+print(requests.get("http://127.0.0.1:5000/modify-events", {"name": "new event"}).text)
